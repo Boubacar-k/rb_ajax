9acc442db4358621ec6ce3b16b7eb887b96d57af
Boubacar-k <boubacark277@gmail.com>
2023-08-25 12:15:14 +0000

    commit 2

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,18 @@ class FavoritesController < ApplicationController
 
   def create
     current_user.favorites.create(blog_id: params[:blog_id])
-    redirect_to blogs_path
+    respond_to do |format|
+      format.html { redirect_to blogs_path }
+      format.js { render :create }
+    end
   end
 
   def destroy
     current_user.favorites.find_by(id: params[:id]).destroy
-    redirect_to blogs_path
+    respond_to do |format|
+      format.html { redirect_to blogs_path }
+      format.js { render :destroy }
+    end
   end
 
 end
